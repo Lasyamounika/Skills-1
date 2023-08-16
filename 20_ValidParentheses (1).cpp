d311e5274decfd73eaa10a1f45fad19d12c1328d
Lasyamounika <141750506+Lasyamounika@users.noreply.github.com>
2023-08-16 21:27:28 +0530

    Add files via upload
    program of 20 valid parentheses checking expression

---
+++ 20_ValidParentheses (1).cpp
@@ -0,0 +1,45 @@
+//creating a program for 20 valid parentheses
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class vp {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '{' || s[i] == '[' || s[i] == '(') {
+                st.push(s[i]);
+            } else {
+                if (st.empty()) {
+                    return false;
+                } else {
+                    if ((st.top() == '{' && s[i] == '}') || 
+                        (st.top() == '[' && s[i] == ']') || 
+                        (st.top() == '(' && s[i] == ')')) {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+int main() {
+    string expression;
+    cin >> expression;
+
+    vp validator; // Create an instance of the vp class
+    if (validator.isValid(expression)) {
+        cout << "Valid expression" << endl;
+    } else {
+        cout << "Invalid expression" << endl;
+    }
+
+    return 0;
+}
